37825fa517e18bf7391a09ed124eee09a4f7584d
vioriacvicentin <vitoriavicentin@hotmail.com>
2021-03-20 21:13:11 -0300

    Hotfix

--- src/pages/flavors/index.tsx
+++ src/pages/flavors/index.tsx
@@ -1,4 +1,4 @@
-import CardFlavors from "@src/componentes/cardFlavors.tsx";
+import CardFlavors from "@src/componentes/cardFlavors";
 import Header from "@src/componentes/Header";
 /* Pagina do sabores */
 export const Flavors = () => {
